59b575097d74c64b0f5cc9e918a615ee308b4415
Sultan Ali <sultanjamali1986@gmail.com>
2022-04-13 16:48:26 +0300

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Sultan Ali on 13/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
